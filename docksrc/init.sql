6ba6d524ad0459a7061312be72f3079db34ea08f
Yamin LEGZOULI <ylegzoul@e2r7p18.42.fr>
2020-02-03 17:43:27 +0100

    map

--- docksrc/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'pswd';
-GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'pswd' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
